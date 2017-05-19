32240bbd1b392988138c81669801abfab5c11006
denisadonis <denisadonis06@gmail.com>
2017-05-19 11:31:44 -0600

    Archivos bases modificados

--- programa.py
+++ programa.py
@@ -3,27 +3,8 @@
 # Autor: Denis Henriquez
 # Fecha: dd/mm/yyyy
 
-def crearArchivo():
-	file = open("temp.dat", "w")
-	file.close()
-
-def escribirArchivo():
-	file = open("temp.dat", "a")
-	file.write(input("?") + "\n")
-	file.close()
-
-def leerArchivo():
-	file = open("temp.dat", "r")
-	line = file.readline()
-	while line != "":
-		print(line - "\n")
-		line = file.readline()
-	print(type(file))
-	file.close()
-
-
 def main():
-	leerArchivo()
+	print("Hello world")
 
 if __name__ == '__main__':
 	main()
