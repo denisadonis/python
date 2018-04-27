37e2d4e48398c9c3bed36e24ef37ff3921007be5
Denis Henriquez <denisadonis06@gmail.com>
2018-04-27 16:12:56 -0600

    reset

--- programa.py
+++ programa.py
@@ -2,20 +2,9 @@
 # Version: 0.0
 # Autor: Denis Henriquez
 # Fecha: 10/10/10
-# prueba 4
-# Otro comentario
 
 def main():
-	print("Hello world")
-	print("otra prueba")
-	
-	print("que es esto")
-<<<<<<< HEAD
-	temp = 10 + 12
-	temp *= temp
-=======
-	temp = 10 + 12 * 10 / 1.7
->>>>>>> fa587308bc5f9137e9144b2ceffd8512426b35bf
+	pass	
 
 if __name__ == '__main__':
 	main()
